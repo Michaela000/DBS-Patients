91491c1d3377062efeb63d62b5d887b585a58f84
dpedrosac <david.pedrosa@staff.uni-marburg.de>
2021-07-07 23:44:01 +0200

    changes in test_pyqt5.py

--- test_pyqt5.py
+++ test_pyqt5.py
@@ -2,23 +2,32 @@
 """Dialog-Style application."""
 
 import sys
+from PyQt5 import QtCore
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtWidgets import QDialogButtonBox
-from PyQt5.QtWidgets import QFormLayout
+from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QGroupBox
+from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QLabel
 
-# Da stimmt etwas noch nicht. Ich erhalte bei beiden Optionen die gleiche Antowrt "Please Try Again!". 
-# Ich schätze es liegt an dieser Zeile "btns.clicked.connect(greeting)". 
-# Aber der Rest läuft. 
+# Hi Michaela, ich habe das mal vom Aufbau geändert. Das GUI wirkt in seiner Form zwar etwas komplexer, aber ist in
+# seiner Logik viel stimmiger alles in allem. Kurze Erklärung: self.layout ist das gesamte Layout und ist vertikal
+# aufgebaut, dann haben wir die erste Box (optionbox1) die allgemeine Daten beinhaltet und es gibt (bisher) eine Leiste
+# mit Knöpfen, die man auch erweitern kann. Darf ich vorschlagen, dass Du versuchst das nachzuvollziehen und erst einmal
+# alles an Inhalten einfügst. Wenn Du soweit bist, kannst Du gerne versuchen eine zweite Box auf der rechten Seite
+# einzufügen (Du kannst Dir vielleicht noch Zeile 292f bei
+# https://github.com/dpedrosac/cDBS/blob/master/utils/settingsNIFTIprocAnts.py anschauen ; ) Die FUnktion greetings
+# lassen wir einfach weg, die brauchen wir nicht)
+
 
 def greeting():
     """Function."""
     if QDialogButtonBox.Cancel:
-        print('Please Try Again!')
+        print('Close')
     else:
         print('Thank You!')
 
@@ -32,24 +41,49 @@ class Dialog(QDialog):
         self.setWindowTitle('Dialog_Test')
         self.setGeometry(200, 100, 280, 170)
         self.move(850, 425)
-        helloMsg = QLabel('<h1>Hello!</h1>', parent=self)
-        helloMsg.move(15, 125)
-        dlgLayout = QVBoxLayout()
-        formLayout = QFormLayout()
-        formLayout.addRow('Name:', QLineEdit())
-        formLayout.addRow('Age:', QLineEdit())
-        formLayout.addRow('Birthdate:', QLineEdit())
-        formLayout.addRow('Job:', QLineEdit())
 
-        dlgLayout.addLayout(formLayout)
-        btns = QDialogButtonBox()
-        btns.setStandardButtons(
-           QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        btns.clicked.connect(greeting)
+        self.layout = QVBoxLayout(self)
+        self.optionbox1 = QGroupBox('Personal data')
+        self.settings_list = QVBoxLayout(self.optionbox1)
 
-        dlgLayout.addWidget(btns)
-        self.setLayout(dlgLayout)
+        # ====================    Create Content for First Option box on Top left      ====================
+        self.subj_name = QLabel('Name:\t\t')
+        self.lineEditFilename = QLineEdit()
 
+        lay1 = QHBoxLayout()
+        lay1.addWidget(self.subj_name)
+        lay1.addWidget(self.lineEditFilename)
+        lay1.addStretch()
+
+        self.subj_age = QLabel('Age:\t\t')
+        self.lineEditFilename = QLineEdit()
+
+        lay2 = QHBoxLayout()
+        lay2.addWidget(self.subj_age)
+        lay2.addWidget(self.lineEditFilename)
+        lay2.addStretch()
+
+        self.settings_list.addLayout(lay1)
+        self.settings_list.addLayout(lay2)
+
+        # ====================    Create Content for Buttons at the Bottom      ====================
+        layout_bottom = QHBoxLayout()
+        self.button_savereturn = QPushButton('Save settings \nand return')
+        self.button_savereturn.clicked.connect(self.onClickedSaveReturn)
+        self.button_close = QPushButton('Save and \nclose')
+        self.button_close.clicked.connect(self.close)  # TODO: define function to save things before closing
+
+        layout_bottom.addStretch(1)
+        layout_bottom.addWidget(self.button_savereturn)
+        layout_bottom.addWidget(self.button_close)
+
+        self.layout.addWidget(self.optionbox1)
+        self.layout.addLayout(layout_bottom)
+
+    # In the next lines, actions are defined when Buttons are pressed
+    @QtCore.pyqtSlot()
+    def onClickedSaveReturn(self):
+        print('here a function is needed to update files')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
