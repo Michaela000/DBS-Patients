12a199373e7c549ec167226fb6f0315f01ea42a9
dpedrosac <david.pedrosa@staff.uni-marburg.de>
2021-07-08 23:37:01 +0200

    changes in test_pyqt5.py

--- test_pyqt5.py
+++ test_pyqt5.py
@@ -1,10 +1,5 @@
 # Filename: test_pyqt5.py
 
-# Hi David. Das war schwieriger als ich gedacht habe. Ich habe eigentlich den ganzen Tag damit verbracht diese Datei zu speichern. Trotzdem klappt das ganze noch nicht
-# 100%, da er zwar das Fenster öffnet zum abspeichern aber nichts abspeichert. 
-# Ich habe noch eine Frage zum Inhalt. Ich dachte, ich darf den Namen und das Alter nicht veröffentlichen aber Du meintest, ich sollte schon einmal die Inhalte einfügen.
-# Tut mir Leid, dass ich im Moment nicht so schnell voran komme. Ich glaube aber, sobald ich eine Box fertig habe, wird das ganze viel schneller gehen. 
-
 """Dialog-Style application."""
 
 import sys
@@ -20,23 +15,18 @@ from PyQt5.QtWidgets import QGroupBox
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QLabel, QFileDialog, QWidget, QMainWindow
 
+# Hey, also alles gut! Ehrlicherweise hast Du viel mehr gemacht, als was ich wollte. Es ging mir eigentlich nur
+# darum, dass Du mal ein wenig mit dem Code spielst und versuchst Inhalt einzufügen. Das mit dem Speichern und
+# Löschen, das bekommen wir alles noch später hin. Ich habe den Eindruck,  es würde Dir mehr bringen, wenn wir erst noch
+# ein Paar Zwischenschritte einfügen, bevor wir mit "decorator" starten (trotzdem mega
+# gut, dass Du das hinbekommen hast). Meine Bitte für die nächsten Tage wäre also, dass Du ein GUI erstellst wie das
+# aus dem Makro der Excel Datei (preoperative) ein GUI für die Medikation (also alle Parkinson Medikamente die es gibt)
+# und ein GUI für die Eingabe der Daten (also der PID), sodass man prüfen kann, ob es den Menschen gibt oder nicht.
+# Zum jetzigen Zeitpunkt noch keine Funktionen und keine Verknüpfungen, reines Aussehen des GUI. Den Rest machen wir
+# noch. Das speicherst Du unter einem Ordner GUI im Hauptverzeichnis.
 
-# Hi Michaela, ich habe das mal vom Aufbau geändert. Das GUI wirkt in seiner Form zwar etwas komplexer, aber ist in
-# seiner Logik viel stimmiger alles in allem. Kurze Erklärung: self.layout ist das gesamte Layout und ist vertikal
-# aufgebaut, dann haben wir die erste Box (optionbox1) die allgemeine Daten beinhaltet und es gibt (bisher) eine Leiste
-# mit Knöpfen, die man auch erweitern kann. Darf ich vorschlagen, dass Du versuchst das nachzuvollziehen und erst einmal
-# alles an Inhalten einfügst. Wenn Du soweit bist, kannst Du gerne versuchen eine zweite Box auf der rechten Seite
-# einzufügen (Du kannst Dir vielleicht noch Zeile 292f bei
-# https://github.com/dpedrosac/cDBS/blob/master/utils/settingsNIFTIprocAnts.py anschauen ; ) Die FUnktion greetings
-# lassen wir einfach weg, die brauchen wir nicht)
-
-
-def greeting():
-    """Function."""
-    if QDialogButtonBox.Cancel:
-        print('Close')
-    else:
-        print('Thank You!')
+# P.S.: Du hast natürlich Recht, kein personenbezogener Inhalt, das war verwirrend von mir. Sorry! Mir ist
+# nichts besseres eingefallen.
 
 
 class Dialog(QDialog):
@@ -73,12 +63,15 @@ class Dialog(QDialog):
         self.settings_list.addLayout(lay1)
         self.settings_list.addLayout(lay2)
 
+        # ====================    Create Content for Second Option box on Top right      ====================
+        # TODO: rechte Seite
+
         # ====================    Create Content for Buttons at the Bottom      ====================
         layout_bottom = QHBoxLayout()
         self.button_savereturn = QPushButton('Save settings \nand return')
         self.button_savereturn.clicked.connect(self.onClickedSaveReturn)
         self.button_close = QPushButton('Save and \nclose')
-        self.button_close.clicked.connect(self.close)  # TODO: define function to save things before closing
+        self.button_close.clicked.connect(self.close)
 
         layout_bottom.addStretch(1)
         layout_bottom.addWidget(self.button_savereturn)
@@ -102,9 +95,10 @@ class Dialog(QDialog):
         print(fileName)
 
     # for opening
-    def open_dialog_box (self):
+    def open_dialog_box(self):
         option = QFileDialog.Options()
-        # first parameter is self; second is the Window Title, third title is Default File Name, fourth is FileType, fifth is options
+        # first parameter is self; second is the Window Title, third title is Default File Name, fourth is FileType,
+        # fifth is options
         file = QFileDialog.getOpenFileName(self, "Save File Window Title", "default.txt", "All Files (*)", options=option)
         print(file)
 
